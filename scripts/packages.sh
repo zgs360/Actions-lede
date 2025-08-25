5f43394382d86280c324729bd27a6f7aa4c5ee6d
zgs360 <zgs360@gmail.com>
2025-08-25 13:56:53 +0800

    Update packages.sh

--- scripts/packages.sh
+++ scripts/packages.sh
@@ -36,7 +36,7 @@ pushd package/small
 #ssrp
 # git clone -b master --depth 1 https://github.com/fw876/helloworld.git
 #passwall
-git clone -b main --depth 1 https://github.com/xiaorouji/openwrt-passwall.git
+#git clone -b main --depth 1 https://github.com/xiaorouji/openwrt-passwall.git
 #passwall2
 git clone -b main --depth 1 https://github.com/xiaorouji/openwrt-passwall2.git
 #mosdns
